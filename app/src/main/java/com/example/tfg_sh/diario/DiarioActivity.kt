a40dd20a7abe5ea026f404c35b6da1b99fddaea3
Hector Fernandez Ruiz <hectorfernandezruiz.02@gmail.com>
2023-04-24 18:44:33 +0200

    Cargar diseño según condición

--- app/src/main/java/com/example/tfg_sh/diario/DiarioActivity.kt
+++ app/src/main/java/com/example/tfg_sh/diario/DiarioActivity.kt
@@ -2,16 +2,27 @@ package com.example.tfg_sh.diario
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import com.example.tfg_sh.databinding.ActivityDiarioBinding
+import com.example.tfg_sh.databinding.ActivityDiarioEmptyBinding
+import com.example.tfg_sh.databinding.ActivityDiarioEditBinding
 
 class DiarioActivity : AppCompatActivity() {
 
-    private lateinit var diario: ActivityDiarioBinding
+    private lateinit var diarioEmpty: ActivityDiarioEmptyBinding
+    private lateinit var diarioEdit: ActivityDiarioEditBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        diario = ActivityDiarioBinding.inflate(layoutInflater)
-        setContentView(diario.root)
+        diarioEmpty = ActivityDiarioEmptyBinding.inflate(layoutInflater)
+        setContentView(diarioEmpty.root)
+
+        // comprobamos si el día tiene un color, si tiene entonces cargamos el otro layout
+        var i = 0
+        if(i == 1){
+            diarioEdit = ActivityDiarioEditBinding.inflate(layoutInflater)
+            setContentView(diarioEdit.root)
+        }
+
+
     }
 
 }
