c43b199de270f80ccdf5e8522d696ee2c40b2d45
sanbricio <sanbriciorojas11@gmail.com>
2023-05-17 19:18:43 +0200

    [FIX] Solución error borrarTodos ToDoListMain

--- app/src/main/java/com/example/tfg_sh/toDoList/ToDoListMain.kt
+++ app/src/main/java/com/example/tfg_sh/toDoList/ToDoListMain.kt
@@ -3,13 +3,18 @@ package com.example.tfg_sh.toDoList
 import android.content.Intent
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.PopupMenu
+import androidx.lifecycle.lifecycleScope
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.tfg_sh.R
 import com.example.tfg_sh.Utils
 import com.example.tfg_sh.bbdd.BetterYouBBDD
+import com.example.tfg_sh.bbdd.dao.BetterYouDao
 import com.example.tfg_sh.databinding.ActivityToDoListMainBinding
+import kotlinx.coroutines.launch
 
 class ToDoListMain : AppCompatActivity() {
 
@@ -36,14 +41,17 @@ class ToDoListMain : AppCompatActivity() {
                         agregarTarea()
                         true
                     }
+
                     R.id.eliminarTareas -> {
-                        //TODO eliminarTareas()
+                        eliminarTareas(dao)
                         true
                     }
+
                     R.id.filtrar -> {
                         //TODO filtrarPorPrioridad()
                         true
                     }
+
                     else -> false
                 }
             }
@@ -55,7 +63,7 @@ class ToDoListMain : AppCompatActivity() {
 
     }//onCreate
 
-    private fun initRecyclerView(){
+    private fun initRecyclerView() {
         val recyclerView = toDoList.tareaRecyclerView
         recyclerView.layoutManager = LinearLayoutManager(this)
         recyclerView.adapter = TareaAdapter(ObjectTarea.getAll())
@@ -76,6 +84,26 @@ class ToDoListMain : AppCompatActivity() {
     private fun agregarTarea() {
         val intent = Intent(this, InsertTareaActivity::class.java)
         startActivity(intent)
+    }
+
+    private fun eliminarTareas(dao: BetterYouDao) {
+        val alertDialog = AlertDialog.Builder(this)
+        alertDialog.setTitle("Eliminar tareas")
+            .setMessage("¿Quieres borrar todas las tareas?")
+            .setCancelable(true)
+            .setPositiveButton("Si") { dialogInterface, it ->
+                ObjectTarea.deleteAll()
+                lifecycleScope.launch {
+                    dao.deleteAllTareas()
+                }
+                initRecyclerView()
+                Toast.makeText(this, "Se han borrado las tareas correctamente", Toast.LENGTH_LONG)
+                    .show()
+            }
+            .setNegativeButton("No") { dialogInterface, it ->
+                dialogInterface.cancel()
+            }
+            .show()
     }
 
 }
