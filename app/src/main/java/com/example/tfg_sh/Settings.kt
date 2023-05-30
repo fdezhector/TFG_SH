c9449ef029ae8e7140bb5fb3c3acc541b5403bc0
sanbricio <sanbriciorojas11@gmail.com>
2023-05-30 20:29:39 +0200

    [FEATURE] OpenFileDialog importar funcional

--- app/src/main/java/com/example/tfg_sh/Settings.kt
+++ app/src/main/java/com/example/tfg_sh/Settings.kt
@@ -1,6 +1,7 @@
 package com.example.tfg_sh
 
 import android.app.Activity
+import android.content.Context
 import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
@@ -12,10 +13,12 @@ import android.widget.ArrayAdapter
 import android.widget.Button
 import android.widget.CheckedTextView
 import android.widget.TextView
+import android.widget.Toast
 import androidx.activity.result.ActivityResult
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.NotificationManagerCompat
 import androidx.core.content.ContextCompat
 import androidx.lifecycle.lifecycleScope
 import com.example.tfg_sh.bbdd.BetterYouBBDD
@@ -23,6 +26,7 @@ import com.example.tfg_sh.bbdd.dao.BetterYouDao
 import com.example.tfg_sh.bbdd.entidades.DatosBBDD
 import com.example.tfg_sh.bbdd.entidades.Tarea
 import com.example.tfg_sh.databinding.ActivitySettingsBinding
+import com.example.tfg_sh.notificacion.NotificacionAlarma.Companion.NOTIFICATION_ID
 import com.google.gson.Gson
 import kotlinx.coroutines.launch
 import java.io.File
@@ -30,6 +34,7 @@ import java.io.File
 class Settings : AppCompatActivity() {
 
     private lateinit var settings: ActivitySettingsBinding
+    val dao = BetterYouBBDD.getInstance(this).betterYouDao
     private val sActivityResultLauncher = registerForActivityResult(
         ActivityResultContracts.StartActivityForResult()
     ) { result: ActivityResult ->
@@ -37,19 +42,41 @@ class Settings : AppCompatActivity() {
             val data: Intent? = result.data
             val uri: Uri? = data?.data
             //Proceso el archivo y veo que hago con el pasarle IMPORTAR-BBDD
-            //importarBBDD(dao)
+            importarBBDD(dao, uri)
         }
     }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         settings = ActivitySettingsBinding.inflate(layoutInflater)
         setContentView(settings.root)
 
-
-
-        val dao = BetterYouBBDD.getInstance(this).betterYouDao
-
         initDropDownMenu()
+
+
+        // FIXME
+        /*settings.horaNotificacionDiario.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(parent: AdapterView<*>, view: View?, position: Int, id: Long) {
+                val selectedItem = parent.getItemAtPosition(position) as String
+
+                cancelNotification(this@Settings)
+
+                val horaNotificacion = Utils.obtenerHora(settings.horaNotificacionDiario.selectedItem.toString())
+                // Notificacion Diario
+                Utils.scheduleNotificacion(
+                    this@Settings,
+                    "Diario BetterYou",
+                    "Escribe en tu diario...",
+                    "¿Cómo te ha ido el día?",
+                    (horaNotificacion.time - Calendar.getInstance().timeInMillis).toInt()
+                )
+            }
+
+            override fun onNothingSelected(parent: AdapterView<*>) {
+                // NO APLICA | Maneja el caso en el que no se haya seleccionado ninguna opción
+            }
+        }*/
+
 
         // TODO
         //  Para importar: se le pedirá que escoja un archivo JSON de su sistema (escalable a que se le pida un archivo JSON de Google Drive)
@@ -108,53 +135,13 @@ class Settings : AppCompatActivity() {
             Utils.goToMainScreen(this)
         }
     }
+
     private fun openFileDialog() {
         val data = Intent(Intent.ACTION_OPEN_DOCUMENT)
         data.addCategory(Intent.CATEGORY_OPENABLE)
-        data.type = "json/*"
+        data.type = "*/*"
         val chooserIntent = Intent.createChooser(data, "Choose a file")
         sActivityResultLauncher.launch(chooserIntent)
-    }
-    private fun exportarBBDD(dao: BetterYouDao){
-        //Log.e("FILEDIR", getExternalFilesDir(null).toString())
-        //Creamos las listas de todas las entidades de la BBDD
-        lifecycleScope.launch{
-            val diarios = dao.getAllDiarios()
-            val eventos = dao.getAllEventos()
-            val tareas = dao.getAllTareas() as List<Tarea>
-            val notas = dao.getAllNotas()
-
-            val datos = DatosBBDD(diarios,eventos,notas,tareas)
-            // Parseo JSON
-            val gson = Gson()
-            val json = gson.toJson(datos)
-            // Creamos el fichero
-            val file = File(this@Settings.getExternalFilesDir(null), "data.json")
-            file.writeText(json)
-        }
-    }
-
-    //FIXME Al importar la BBDD el recycler view de las tareas no se sincroniza
-
-    private fun importarBBDD(dao: BetterYouDao) {
-        try {
-            // Obtenemos la referencia al archivo JSON en el almacenamiento externo.
-            val file = File(this.getExternalFilesDir(null), "data.json")
-            // Leemos el archivo
-            val jsonString = file.readText()
-            // Convertimos el archivo en objetos de tipo DatosBBDD
-            val gson = Gson()
-            val datos = gson.fromJson(jsonString, DatosBBDD::class.java)
-            // Insertamos todos los datos en la BBDD
-            lifecycleScope.launch {
-                dao.insertAllDiarios(datos.diarios)
-                dao.insertAllEventos(datos.eventos)
-                dao.insertAllTareas(datos.tareas)
-                dao.insertAllNotas(datos.notas)
-            }
-        } catch (e: Exception) {
-            Log.e("ImportarBBDD", "Error al importar los datos desde el archivo JSON", e)
-        }
     }
 
     fun initDropDownMenu() {
@@ -167,19 +154,102 @@ class Settings : AppCompatActivity() {
         settings.horaNotificacionDiario.adapter = adaptador
 
         // listener que se ejectutará cuando se ha seleccionado una prioridad
-        settings.horaNotificacionDiario.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
-            override fun onItemSelected(parent: AdapterView<*>, view: View?, position: Int, id: Long) {
-                val selectedItem = parent.getItemAtPosition(position) as String
+        settings.horaNotificacionDiario.onItemSelectedListener =
+            object : AdapterView.OnItemSelectedListener {
+                override fun onItemSelected(
+                    parent: AdapterView<*>,
+                    view: View?,
+                    position: Int,
+                    id: Long
+                ) {
+                    val selectedItem = parent.getItemAtPosition(position) as String
 
-                val dropDownShownItem = this@Settings.findViewById<CheckedTextView>(R.id.dropdown_menu_layout)
-                dropDownShownItem.setTextColor(ContextCompat.getColor(this@Settings, R.color.gris))
-                dropDownShownItem.setBackgroundColor(ContextCompat.getColor(this@Settings, R.color.negro))
-            }
+                    val dropDownShownItem =
+                        this@Settings.findViewById<CheckedTextView>(R.id.dropdown_menu_layout)
+                    dropDownShownItem.setTextColor(
+                        ContextCompat.getColor(
+                            this@Settings,
+                            R.color.gris
+                        )
+                    )
+                    dropDownShownItem.setBackgroundColor(
+                        ContextCompat.getColor(
+                            this@Settings,
+                            R.color.negro
+                        )
+                    )
+                }
 
-            override fun onNothingSelected(parent: AdapterView<*>) {
-                // NO APLICA | Maneja el caso en el que no se haya seleccionado ninguna opción
+                override fun onNothingSelected(parent: AdapterView<*>) {
+                    // NO APLICA | Maneja el caso en el que no se haya seleccionado ninguna opción
+                }
             }
+    }
+
+    private fun exportarBBDD(dao: BetterYouDao) {
+        //Log.e("FILEDIR", getExternalFilesDir(null).toString())
+        //Creamos las listas de todas las entidades de la BBDD
+        lifecycleScope.launch {
+            val diarios = dao.getAllDiarios()
+            val eventos = dao.getAllEventos()
+            val tareas = dao.getAllTareas() as List<Tarea>
+            val notas = dao.getAllNotas()
+
+            val datos = DatosBBDD(diarios, eventos, notas, tareas)
+            // Parseo JSON
+            val gson = Gson()
+            val json = gson.toJson(datos)
+            // Creamos el fichero
+            val file = File(this@Settings.getExternalFilesDir(null), "data.json")
+            file.writeText(json)
         }
+    }
+
+    //FIXME Al importar la BBDD el recycler view de las tareas no se sincroniza
+
+    private fun importarBBDD(dao: BetterYouDao, uri: Uri?) {
+        try {
+            if (uri == null) {
+                Toast.makeText(this, "No se seleccionó ningún archivo", Toast.LENGTH_LONG).show()
+                return
+            }
+
+            var filePath: String? = uri.path
+            if (filePath == null) {
+                Toast.makeText(this, "No se puede obtener la ruta del archivo", Toast.LENGTH_LONG)
+                    .show()
+                return
+            }
+
+            filePath = filePath.replace("/document/raw:", "")
+
+            // Obtenemos la referencia al archivo JSON en el almacenamiento externo.
+            val file = File(filePath)
+
+            // Leemos el archivo
+            val jsonString = file.readText()
+            // Convertimos el archivo en objetos de tipo DatosBBDD
+            val gson = Gson()
+            val datos = gson.fromJson(jsonString, DatosBBDD::class.java)
+            // Insertamos todos los datos en la BBDD
+            lifecycleScope.launch {
+                dao.insertAllNotas(datos.notas)
+                dao.insertAllDiarios(datos.diarios)
+                dao.insertAllEventos(datos.eventos)
+                dao.insertAllTareas(datos.tareas)
+            }
+        } catch (e: Exception) {
+            Log.e("ImportarBBDD", "Error al importar los datos desde el archivo JSON", e)
+        }
+    }
+
+    // Borrar la notificación
+    private fun cancelNotification(context: Context) {
+        // Obtener el NotificationManager
+        val notificationManager = NotificationManagerCompat.from(context)
+
+        // Cancelar la notificación
+        notificationManager.cancel(NOTIFICATION_ID)
     }
 
 }
